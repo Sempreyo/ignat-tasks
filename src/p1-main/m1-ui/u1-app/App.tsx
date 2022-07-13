050092634a3dd645a40c9eb267b1133efe060dbb
ArtZ <snickgem@gmail.com>
2022-07-13 20:47:28 +0400

    hw2

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import s from './App.module.css'
 import HW1 from '../../../p2-homeworks/h1/HW1'
+import HW2 from '../../../p2-homeworks/h2/HW2';
 
 function App() {
 	return (
 		<div className={s.App}>
 			<div className={s.block}>
 				<div className={s.title}>Homework 1</div>
+				<HW1/>
 			</div>
-			<HW1/>
-			{/*<HW2/>*/}
+			<div className={s.block}>
+				<div className={s.title}>Homework 2</div>
+				<HW2/>
+			</div>
 			{/*<HW3/>*/}
 			{/*<HW4/>*/}
 			{/*<HW5/>*/}
--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,38 +1,51 @@
 import React from 'react'
 import Affair from './Affair'
-import {AffairType} from './HW2'
+import s from './Affairs.module.css'
+import {AffairType, FilterType} from './HW2'
 
 type AffairsPropsType = { // need to fix any
-    data: any
-    setFilter: any
-    deleteAffairCallback: any
+	data: Array<AffairType>
+	setFilter: (filter: FilterType) => void
+	deleteAffairCallback: (_id: number) => void
 }
 
 function Affairs(props: AffairsPropsType) {
-    const mappedAffairs = props.data.map((a: AffairType) => (
-        <Affair // should work
-            key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
-            affair={a}
-            deleteAffairCallback={props.deleteAffairCallback}
-        />
-    ))
+	const mappedAffairs = props.data.map((a: AffairType) => (
+		<Affair // should work
+			key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
+			affair={a}
+			deleteAffairCallback={props.deleteAffairCallback}
+		/>
+	))
 
-    const setAll = () => {} // need to fix
-    const setHigh = () => {}
-    const setMiddle = () => {}
-    const setLow = () => {}
+	const setAll = () => {
+		props.setFilter('all');
+	} // need to fix
+	const setHigh = () => {
+		props.setFilter('high');
+	}
+	const setMiddle = () => {
+		props.setFilter('middle');
+	}
+	const setLow = () => {
+		props.setFilter('low');
+	}
 
-    return (
-        <div>
+	return (
+		<div className={s.container}>
 
-            {mappedAffairs}
+			<ul className={s.list}>
+				{mappedAffairs}
+			</ul>
 
-            <button onClick={setAll}>All</button>
-            <button onClick={setHigh}>High</button>
-            <button onClick={setMiddle}>Middle</button>
-            <button onClick={setLow}>Low</button>
-        </div>
-    )
+      <div className={s.filterBtns}>
+        <button className={s.filterBtn} onClick={setAll}>All</button>
+        <button className={s.filterBtn} onClick={setHigh}>High</button>
+        <button className={s.filterBtn} onClick={setMiddle}>Middle</button>
+        <button className={s.filterBtn} onClick={setLow}>Low</button>
+      </div>
+		</div>
+	)
 }
 
 export default Affairs
